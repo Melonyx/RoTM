d28925169a0cae1844565f1520f50860c9d02188
George Loginov <egorloginov857@gmail.com>
2020-01-19 19:07:24 +0400

    Minor fixes

--- Assets/LevelsListController.cs
+++ Assets/LevelsListController.cs
@@ -10,18 +10,25 @@ public class LevelsListController : MonoBehaviour
     public Button _listItemRef;
     public Levels levelsInfo;
 
+    private Transform _parent;
+
     void Start()
     {
+        _parent = _listItemRef.transform.parent;
+
         foreach (var levelInfo in levelsInfo.levels)
         {
-            var item = Instantiate(_listItemRef, _listItemRef.transform.parent);
+            var item = Instantiate(_listItemRef, _parent);
             item.GetComponentInChildren<TextMeshProUGUI>().text = levelInfo.name;
-            item.onClick.AddListener(() =>
-            {
-                StartCoroutine(LoadLevel(levelInfo.scene));
-            });
+            item.onClick.AddListener(() => OnButtonClicked(levelInfo));
         }
         Destroy(_listItemRef.gameObject);
+    }
+
+    private void OnButtonClicked(LevelInfo levelInfo)
+    {
+        StartCoroutine(LoadLevel(levelInfo.scene));
+        _parent.gameObject.SetActive(false);
     }
 
     private IEnumerator LoadLevel(string name)
